a87f86d653ff560284a3f46328490ce6c33fda78
SimaoCazola <cazolastudio@gmail.com>
2023-07-14 01:14:23 +0100

    Datepicker Countries and Cities

--- SuperShop/Data/ICountryRepository.cs
+++ SuperShop/Data/ICountryRepository.cs
@@ -1,11 +1,33 @@
 ﻿using SuperShop.Data.Entities;
+using SuperShop.Models;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace SuperShop.Data
 {
     public interface ICountryRepository : IGenericRepository<Country>
     {
+        // Metodo para retornar todas os Paises e cidas 
+        IQueryable GetCountriesWithCities();
 
 
+        // Metodo que retorna cidades e pais por ID
+        Task<Country> GetCountriesWithCitiesAsync(int id);
+
+
+        //Metodo que retorna a cidade por ID
+        Task<City> GetCityAsync(int id); 
+
+
+        // Metodo que retorna o modelo para mostrar na view
+        Task AddCityAsync(CityViewModel model);
+
+
+        //Metodo que retornar a alteracao das cidades
+        Task<int> UpdateCityAsync(City city);
+
+        //Metodo que retornar o apagar cidade
+        Task<int> DeleteCityAsync(City city);
 
     }
 }
