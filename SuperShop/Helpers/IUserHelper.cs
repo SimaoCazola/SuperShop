76baa5429b512299da751993890b8935caac5879
SimaoCazola <cazolastudio@gmail.com>
2023-07-22 02:18:36 +0100

    Create Tokens

--- SuperShop/Helpers/IUserHelper.cs
+++ SuperShop/Helpers/IUserHelper.cs
@@ -22,5 +22,18 @@ namespace SuperShop.Helpers
         Task CheckRoleAsync(string roleName);
         Task AddUserToRoleAsync(User user, string roleName);
         Task<bool> IsUserInRoleAsync(User user, string roleName);
+
+        // Metodo que valida a password do utilizador
+        Task<SignInResult> ValidatePasswordAsync(User user, string password);
+
+        //Task<string> GenerateEmailConfirmationTokenAsync(User user);
+
+        //Task<IdentityResult> ConfirmEmailAsync(User user, string token);
+
+        //Task<User> GetUserByIdAsync(string userId);
+
+        //Task<string> GeneratePasswordResetTokenAsync(User user);
+
+        //Task<IdentityResult> ResetPasswordAsync(User user, string token, string password);
     }
 }
